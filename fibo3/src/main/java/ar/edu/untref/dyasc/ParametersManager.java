f46de77dd39692d8771dd754082aeaf892ba693f
oargueyo <oaargueyo@gmail.com>
2019-10-05 22:37:53 -0300

    ParametersManager agregado seteo de largo de la secuencia

--- fibo3/src/main/java/ar/edu/untref/dyasc/ParametersManager.java
+++ fibo3/src/main/java/ar/edu/untref/dyasc/ParametersManager.java
@@ -3,7 +3,7 @@ package ar.edu.untref.dyasc;
 public class ParametersManager {
 
     private String[] parameters;
-    private int sequence_length;
+    private int sequenceLength;
 
     ParametersManager(String[] params){
         this.setParameters(params);
@@ -18,6 +18,24 @@ public class ParametersManager {
     }
 
     public void processParameters(){
+        for (String param :this.getParameters()){
+            this.analizeParameter(param);
+        }
+    }
 
+    private void analizeParameter(String parameter) {
+        if(!parameter.contains("-")){
+            this.setSequenceLength(Integer.parseInt(parameter));
+        }else {
+            System.out.println("Esto es un parametro con opciones");
+        }
+    }
+
+    public int getSequenceLength() {
+        return sequenceLength;
+    }
+
+    public void setSequenceLength(int sequence_length) {
+        this.sequenceLength = sequence_length;
     }
 }
--- fibo3/src/test/java/ar/edu/untref/dyasc/ParametersManagerTest.java
+++ fibo3/src/test/java/ar/edu/untref/dyasc/ParametersManagerTest.java
@@ -1,14 +1,17 @@
 package ar.edu.untref.dyasc;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class ParametersManagerTest {
 
     @Test
     public void analizarLargoSecuencia(){
         String[] args = {"1"};
+        int sequenceLength = 1;
         ParametersManager manager = new ParametersManager(args);
-
         manager.processParameters();
+
+        assertEquals(manager.getSequenceLength() , sequenceLength);
     }
 }
